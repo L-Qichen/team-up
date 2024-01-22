68652093b3d3a9b7b89794c6e8c1d6c4792abd42
Toby <5722599@gmail.com>
2024-01-22 16:49:56 -0500

    CourseList component layout is done

---
+++ src/components/CourseList.jsx
@@ -0,0 +1,124 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import '../assets/css/CourseList.css'
+
+const CourseList = () => {
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 4 }}>
+        <Paper
+          className='paper-container'
+          elevation={0}
+          sx={{
+            my: 1,
+            mx: 'auto',
+            p: 2,
+            marginBottom: '1.5rem',
+          }}
+        >
+          <Grid container wrap="nowrap" spacing={3}>
+            <Grid item>
+              <Avatar>W</Avatar>
+            </Grid>
+            <Grid item xs zeroMinWidth>
+              <Typography noWrap>
+                Radar System for Indoor Health Monitoring (A)
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+        <Paper
+          className='paper-container'
+          elevation={0}
+          sx={{
+            my: 1,
+            mx: 'auto',
+            p: 2,
+            marginBottom: '1.5rem',
+          }}
+        >
+          <Grid container wrap="nowrap" spacing={3}>
+            <Grid item>
+              <Avatar>W</Avatar>
+            </Grid>
+            <Grid item xs zeroMinWidth>
+              <Typography noWrap>
+                Radar System for Indoor Health Monitoring (A)
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+        <Paper
+          className='paper-container'
+          elevation={0}
+          sx={{
+            my: 1,
+            mx: 'auto',
+            p: 2,
+            marginBottom: '1.5rem',
+          }}
+        >
+          <Grid container wrap="nowrap" spacing={3}>
+            <Grid item>
+              <Avatar>W</Avatar>
+            </Grid>
+            <Grid item xs zeroMinWidth>
+              <Typography noWrap>
+                Radar System for Indoor Health Monitoring (A)
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+        <Paper
+          className='paper-container'
+          elevation={0}
+          sx={{
+            my: 1,
+            mx: 'auto',
+            p: 2,
+            marginBottom: '1.5rem',
+          }}
+        >
+          <Grid container wrap="nowrap" spacing={3}>
+            <Grid item>
+              <Avatar>W</Avatar>
+            </Grid>
+            <Grid item xs zeroMinWidth>
+              <Typography noWrap>
+                Radar System for Indoor Health Monitoring (A)
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+        <Paper
+          className='paper-container'
+          elevation={0}
+          sx={{
+            my: 1,
+            mx: 'auto',
+            p: 2,
+            marginBottom: '1.5rem',
+          }}
+        >
+          <Grid container wrap="nowrap" spacing={3}>
+            <Grid item>
+              <Avatar>W</Avatar>
+            </Grid>
+            <Grid item xs zeroMinWidth>
+              <Typography noWrap>
+                Radar System for Indoor Health Monitoring (A)
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>
+    </>
+  );
+}
+
+export default CourseList;
--- src/pages/MyProjects.jsx
+++ src/pages/MyProjects.jsx
@@ -1,7 +1,5 @@
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import { Header, Footer } from '../components'
+import { Header, Footer, CourseList } from '../components'
 import '../assets/css/CommonStyle.css'
 
 const MyProjects = () => {
@@ -14,25 +12,9 @@ const MyProjects = () => {
         </h1>
         <div className='break-line' />
       </Container>
+
       <Container maxWidth="md">
-        <Box>
-          <Typography variant="h4"
-            gutterBottom
-            sx={{
-              fontWeight: "700",
-              fontSize: "25px"
-            }}
-          >
-            General Information
-          </Typography>
-          <Typography
-            variant="body1"
-            gutterBottom
-            sx={{ marginBottom: '2rem' }}
-          >
-            Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.
-          </Typography>
-        </Box>
+        <CourseList />
       </Container>
       <Footer />
     </>
