3098400f92a864facfe6416cfd5a18a680c16dd7
Toby <5722599@gmail.com>
2024-02-18 11:55:22 -0500

    Added fetch dada function to get project list from back-end in ProjectList.

--- src/pages/ProjectList.jsx
+++ src/pages/ProjectList.jsx
@@ -2,11 +2,20 @@ import Container from '@mui/material/Container';
 import { Header, Footer, CourseList } from '../components'
 import '../assets/css/CommonStyle.css'
 import { useState, useEffect } from 'react';
+import { customFetch } from '../utils';
 
 const ProjectList = () => {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
+    // const fetchProjectList = async () => {
+    //   try {
+    //     const response = await customFetch.get('/project/projectList');
+    //   } catch (error) {
+    //     console.error('Error fetching project list:', error);
+    //   }
+    // };
+    // fetchProjectList();
     setProjects([
       { id: 1, projectName: 'First Project (A)' },
       { id: 2, projectName: 'One Project (B)' },
