ae4637de47b414ca33a6711327ad9c000b892ebb
Toby <5722599@gmail.com>
2024-03-01 22:16:49 -0500

    Added UserDetails component. And fixed some style bugs of other components

---
+++ src/components/UserDetails.jsx
@@ -0,0 +1,127 @@
+import { useState } from 'react';
+import Container from '@mui/material/Container';
+import '../assets/css/CommonStyle.css'
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+const UserDetails = () => {
+
+  const [user, setUser] = useState({
+    fullName: 'FirstName LastName',
+    username: 'Toby',
+    userRole: 'student',
+    email: '10442734856@gmail.com'
+  });
+
+  const [opnDialogName, setOpenDialogName] = useState(null);
+
+  const closeDialog = () => {
+    setOpenDialogName(null);
+  };
+
+  const openEditUserInfoDialog = () => {
+    setOpenDialogName('UserInfo');
+  }
+
+  return (
+    <>
+      <Container maxWidth="md">
+        <h2 className='h2-title'>
+          User Details
+        </h2>
+        <div className='break-line' />
+      </Container>
+
+      <Container maxWidth="md">
+        <Accordion defaultExpanded
+          sx={{ outline: '1px solid rgba(0,0,0,.2)' }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <Typography variant="h6">
+              Personal Info
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Grid container alignItems="center">
+              <Grid item xs={3} md={2}>
+                <Typography>
+                  Full Name:
+                </Typography>
+              </Grid>
+              <Grid item xs={9} md={10}
+                sx={{ color: '#666' }}
+              >
+                <Typography>
+                  {user.fullName}
+                </Typography>
+              </Grid>
+
+              <Grid item xs={3} md={2}>
+                <Typography>
+                  User Name:
+                </Typography>
+              </Grid>
+              <Grid item xs={9} md={10}
+                sx={{ color: '#666' }}
+              >
+                <Typography>
+                  {user.username}
+                </Typography>
+              </Grid>
+
+              <Grid item xs={3} md={2}>
+                <Typography>
+                  User Role:
+                </Typography>
+              </Grid>
+              <Grid item xs={9} md={10}
+                sx={{ color: '#666' }}
+              >
+                <Typography>
+                  {user.userRole}
+                </Typography>
+              </Grid>
+
+              <Grid item xs={3} md={2}>
+                <Typography>
+                  Email:
+                </Typography>
+              </Grid>
+              <Grid item xs={9} md={10}
+                sx={{ color: '#666' }}
+              >
+                <Typography>
+                  {user.email}
+                </Typography>
+              </Grid>
+            </Grid>
+          </AccordionDetails>
+          <AccordionActions>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <button
+                  type='button'
+                  className='edit-btn'
+                  onClick={openEditUserInfoDialog}
+                >
+                  Edit
+                </button>
+              </Grid>
+            </Grid>
+          </AccordionActions>
+        </Accordion>
+      </Container >
+    </>
+  );
+}
+
+export default UserDetails;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,35 @@
+import { Header, Footer, UserDetails } from '../components'
+import Container from '@mui/material/Container';
+import '../assets/css/CommonStyle.css'
 
 const Profile = () => {
 
   return (
     <>
-      <h1>Profile page</h1>
+      <Header />
+      <Container maxWidth="md">
+        <h1 className='title'>
+          Profile
+        </h1>
+        <div className='break-line' />
+      </Container>
+
+      <UserDetails />
+
+      <Container maxWidth="md">
+        <h2 className='title'>
+          Leaded Projects
+        </h2>
+        <div className='break-line' />
+      </Container>
+
+      <Container maxWidth="md">
+        <h2 className='title'>
+          Enrolled Projects
+        </h2>
+        <div className='break-line' />
+      </Container>
+      <Footer />
     </>
   );
 }
