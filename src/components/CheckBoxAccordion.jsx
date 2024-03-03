53e90f628daf338cc207795cca0cf3a59b4c3762
Toby <5722599@gmail.com>
2024-03-02 23:50:38 -0500

    CheckBoxAccordion component now can fetch the leaded projects list data.

--- src/components/CheckBoxAccordion.jsx
+++ src/components/CheckBoxAccordion.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { customFetch } from '../utils';
 import Container from '@mui/material/Container';
 import '../assets/css/CommonStyle.css'
 import Accordion from '@mui/material/Accordion';
@@ -19,6 +20,24 @@ import Avatar from '@mui/material/Avatar';
 const CheckBoxAccordion = ({ title, user }) => {
 
   const [checked, setChecked] = useState([]);
+  const [projects, setProjects] = useState([
+    { id: 1, projectName: 'Project 1' },
+    { id: 2, projectName: 'Project 2' },
+    { id: 3, projectName: 'Project 3' },
+  ]);
+
+  useEffect(() => {
+    const fetchLeadedProjects = async () => {
+      try {
+        const response = await customFetch.get(`project/getAllLeadedProject?userId=${user.userId}`);
+        setProjects(response.data);
+      } catch (error) {
+        console.error('Error fetching leaded projects:', error);
+      }
+    };
+
+    fetchLeadedProjects();
+  }, [user.id]);
 
   const handleToggle = (value) => () => {
     const currentIndex = checked.indexOf(value);
@@ -43,68 +62,70 @@ const CheckBoxAccordion = ({ title, user }) => {
       </Container>
 
       <Container maxWidth="md">
-        {/* 
-        这里先获取到当前 user的 leaded projects list，
-        然后用map生成多个 Accordion
-         */}
-        <Accordion
-          sx={{ outline: '1px solid rgba(0,0,0,.2)' }}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1-content"
-            id="panel1-header"
-          >
-            <Typography variant="h6">
-              Project 1
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <List dense sx={{ width: '100%', bgcolor: 'background.paper' }}>
-              {/* 通过 project ID 获取 candidate list */}
-              {[0, 1, 2, 3].map((value) => {
-                const labelId = `checkbox-list-secondary-label-${value}`;
-                return (
-                  <ListItem
-                    key={value}
-                    secondaryAction={
-                      <Checkbox
-                        edge="end"
-                        onChange={handleToggle(value)}
-                        checked={checked.indexOf(value) !== -1}
-                        inputProps={{ 'aria-labelledby': labelId }}
-                      />
-                    }
-                    disablePadding
-                  >
-                    <ListItemButton>
-                      <ListItemAvatar>
-                        <Avatar
-                          alt={`Avatar n°${value + 1}`}
-                          src={`/static/images/avatar/${value + 1}.jpg`}
-                        />
-                      </ListItemAvatar>
-                      <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-                    </ListItemButton>
-                  </ListItem>
-                );
-              })}
-            </List>
-          </AccordionDetails>
-          <AccordionActions>
-            <Grid container justifyContent="center">
-              <Grid item>
-                <button
-                  type='button'
-                  className='edit-btn'
-                // onClick={ }
-                >
-                  Confirm
-                </button>
-              </Grid>
-            </Grid>
-          </AccordionActions>
-        </Accordion>
+        {projects.map((project) => {
+          return (
+            <Accordion
+              key={project.id}
+              sx={{ outline: '1px solid rgba(0,0,0,.2)' }}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+                <Typography variant="h6">
+                  {project.projectName}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <List dense sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                  {/* 通过 project ID 获取 candidate list */}
+                  {[0, 1, 2, 3].map((value) => {
+                    const labelId = `checkbox-list-secondary-label-${value}`;
+                    return (
+                      <ListItem
+                        key={value}
+                        secondaryAction={
+                          <Checkbox
+                            edge="end"
+                            onChange={handleToggle(value)}
+                            checked={checked.indexOf(value) !== -1}
+                            inputProps={{ 'aria-labelledby': labelId }}
+                          />
+                        }
+                        disablePadding
+                      >
+                        <ListItemButton>
+                          <ListItemAvatar>
+                            <Avatar
+                              alt={`Avatar n°${value + 1}`}
+                              src={`/static/images/avatar/${value + 1}.jpg`}
+                            />
+                          </ListItemAvatar>
+                          <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                        </ListItemButton>
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              </AccordionDetails>
+              <AccordionActions>
+                <Grid container justifyContent="center">
+                  <Grid item>
+                    <button
+                      type='button'
+                      className='edit-btn'
+                    // onClick={ }
+                    >
+                      Confirm
+                    </button>
+                  </Grid>
+                </Grid>
+              </AccordionActions>
+            </Accordion>
+          );
+        })}
+
       </Container >
     </>
   );
